371099356b5730577cf3e10b19eb5501c426ff4a
youhaolin <cador.ai@aliyun.com>
2018-04-28 08:20:10 +0800

    upload codes.

---
+++ listener.py
@@ -0,0 +1,28 @@
+import asyncio
+from nats.aio.client import Client as NATS
+import utils as tool
+
+
+async def listen(loop):
+    nc = NATS()
+    await nc.connect(io_loop=loop, servers=[tool.NAT_URL])
+
+    async def message_handler(msg):
+        subject = msg.subject
+        reply = msg.reply
+        data = msg.data.decode()
+        print("Received a message on '{subject} {reply}': {data}".format(
+            subject=subject, reply=reply, data=data))
+
+    await nc.subscribe(tool.REPLY_TO, cb=message_handler)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        asyncio.ensure_future(listen(loop))
+        loop.run_forever()
+    except Exception as e:
+        raise e
+    finally:
+        loop.close()
