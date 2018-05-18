0d192dbbcbbb3955baf6d8742c9294c52d24a7d3
cador <cador.ai@aliyun.com>
2018-05-18 02:14:01 +0000

    update shell script.

--- shell-script/getStatus.sh
+++ shell-script/getStatus.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+i=1
 args=''
-for i in `seq 2 $#`
+for var in "$@"
 do
-    args=$args' '${!i}
+    if [ $i -ne 1 ] ; then
+       args=$args' '$var
+    fi
+
+    if [ $i -eq 1 ] ; then
+       i=0
+    fi
 done
+
 exec_str='node query-ad-status.js '$1' '$args
 cd nodejs/script
 eval $exec_str
